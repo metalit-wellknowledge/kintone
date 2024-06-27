03e834a8e539e7347a4d435e99d99023aa16902c
Metal IT <michaerel_0611@yahoo.co.jp>
2024-06-27 11:45:17 +0900

    Create get_apps_all_records.js

---
+++ get_apps_all_records.js
@@ -0,0 +1,26 @@
+// use       : https://js.cybozu.com/kintone-rest-api-client/5.5.1/KintoneRestAPIClient.min.js
+// reference : https://cdn.jsdelivr.net/gh/metalit-wellknowledge/kintone/get_apps_all_records.js
+console.log("GOOD");
+
+(() => {
+  'use strict';
+  kintone.events.on('app.record.index.show', async (event) => {
+    try {
+      // クライアントの作成
+      const client = new KintoneRestAPIClient();
+
+      // リクエストパラメータの設定
+      const APP_ID = 6;
+      const params = {
+        app: APP_ID,
+      };
+
+      // レコードの取得
+      // レコードメソッドで使用できるもの一覧：https://github.com/kintone/js-sdk/blob/main/packages/rest-api-client/docs/record.md
+      const resp = await client.record.getAllRecordsWithId(params);
+      console.log(resp.record);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+})();
