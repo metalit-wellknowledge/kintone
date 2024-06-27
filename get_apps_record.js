0676beef6b9d57efcbee277135b9f6a8e6713a22
Metal IT <michaerel_0611@yahoo.co.jp>
2024-06-27 11:25:45 +0900

    Update get_apps_record.js

--- get_apps_record.js
+++ get_apps_record.js
@@ -1,3 +1,4 @@
+// use       : https://js.cybozu.com/kintone-rest-api-client/5.5.1/KintoneRestAPIClient.min.js
 // reference : https://cdn.jsdelivr.net/gh/metalit-wellknowledge/kintone/get_apps_record.js
 
 const body = {
