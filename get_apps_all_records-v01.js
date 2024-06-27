41c55a24f94c4b6acae93cd7967e44dcd2c6c4da
Metal IT <michaerel_0611@yahoo.co.jp>
2024-06-27 11:48:19 +0900

    Update and rename get_apps_all_records.js to get_apps_all_records-v01.js

--- get_apps_all_records-v01.js
+++ get_apps_all_records-v01.js
@@ -17,7 +17,7 @@ console.log("GOOD");
 
       // レコードの取得
       // レコードメソッドで使用できるもの一覧：https://github.com/kintone/js-sdk/blob/main/packages/rest-api-client/docs/record.md
-      const resp = await client.record.getAllRecordsWithId(params);
+      const resp = await client.record.getAllRecords(params);
       console.log(resp.record);
     } catch (err) {
       console.log(err);
